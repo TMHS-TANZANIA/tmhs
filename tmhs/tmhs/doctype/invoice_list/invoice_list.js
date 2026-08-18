8c59993be98a57320de645de3fcb361db0fb94dd
Daniel <danielbraganza2000@gmail.com>
2026-08-18 15:30:14 +0300

    Implementation of Invoice List

---
+++ tmhs/tmhs/doctype/invoice_list/invoice_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, James Meshack and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Invoice List", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tmhs/tmhs/doctype/invoice_list/invoice_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, James Meshack and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class InvoiceList(Document):
+	pass
